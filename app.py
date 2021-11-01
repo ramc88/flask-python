314983034a90de3b95769c67f18546ba2f5412b2
Ramiro <rclavijo@paypal.com>
2021-11-01 17:27:00 -0300

    init

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask import jsonify
+from flask import request
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello World!"
+@app.route("/", methods=['GET'])
+def sample():
+    return "Example get api"
 
+@app.route("/coffees", methods=['GET'])
+def getCoffes():
+    response = requests.get("https://api.sampleapis.com/coffee/hot")
+    return jsonify({'info': 'Info from https://sampleapis.com/api-list/coffee, TAGSI group flask demo app', 'data': response.json()})
---
+++ readme.md
@@ -0,0 +1,8 @@
+### Install and execute
+
+## Virtualenv
+python3 -m venv venv
+activate: source venv/bin/activate
+
+Install `pip install -r requirements.txt`
+Execute `flask run` available in port 5000
